1d13a7f0ffebaf330437d201e003a75bb5f4afa8
Chenhao Wang <implexrr@gmail.com>
2025-05-02 18:39:29 -0400

    feat: add prelim fetch/context stuff

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,68 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createContext, useEffect, useState } from "react";
 
 import "../assets/styles/App.css"
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import routes from './routes/routes.tsx'
 
 const router = createBrowserRouter(routes);
-const App = () => {
-  return <RouterProvider router={router} />;
+
+export const ShopContext = createContext({
+  addToCart: () => {},
+  cartItems: [],
+  error: null as null | string,
+  loading: true,
+  products: []
+}
+);
+
+export const App = () => {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState<null | string>(null);
+  const [loading, setLoading] = useState(true);
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = () => {
+    // TDL
+  };
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const cached = localStorage.getItem('products');
+      if (cached) {
+        console.log('products already fetched, getting products from localStorage')
+        setProducts(JSON.parse(cached));
+        setLoading(false);
+        return;
+      }
+
+      try {
+        console.log('fetching products')
+        const request = await fetch("https://mock.shop/api?query={products(first:%2030){edges%20{node%20{id%20title%20description%20featuredImage%20{id%20url}%20variants(first:%203){edges%20{node%20{price%20{amount%20currencyCode}}}}}}}}");
+        if (request.status >= 400) throw new Error("server error");
+        const response = await request.json();
+        const newProducts = response.data.products.edges;
+        setProducts(newProducts);
+        localStorage.setItem('products', JSON.stringify(newProducts));
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+        } else {
+          setError("An unknown error occurred");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <ShopContext.Provider value={{ addToCart, cartItems, error, loading, products }}>
+      <RouterProvider router={router} />
+    </ShopContext.Provider>
+  );
 };
 
 export default App;
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,4 +1,11 @@
+import { useContext } from "react";
+
+import { ShopContext } from "../app/App";
+
 const Products = () => {
+  console.log('loading products from shop context... here they are:')
+  const { products } = useContext(ShopContext);
+  console.log(products)
   return (
     <div>Products</div>
   )

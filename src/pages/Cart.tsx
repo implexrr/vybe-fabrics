1af5bb610ccadfaa0f4a3707463179b23ea6834a
implexrr <implexrr@gmail.com>
2025-07-09 23:50:30 -0400

    feat: add cart item totals

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -3,20 +3,51 @@ import { useContext } from "react";
 import { ShopContext } from "../app/context/ShopContext";
 import CartItemCard from "../components/CartItemCard";
 
+const TAX = 0.13;
+
 const Cart = () => {
-  console.log('loading cart from shop context... here it is:');
   const { cartItems } = useContext(ShopContext);
-  console.log(cartItems);
   return (
-    <div className="cartItemCardsContainer">
-      {
-        cartItems.map((cartItem) => {
-          return (
-            <CartItemCard cartItem={cartItem} key={cartItem.id}></CartItemCard>
-          )
-        })
-      }
-    </div>
+    <>
+      <div className="cartItemCards">
+        {
+          cartItems.map((cartItem) => {
+            return (
+              <CartItemCard cartItem={cartItem} key={cartItem.id}></CartItemCard>
+            )
+          })
+        }
+      </div>
+      <div className="cartItemTotals">
+        <div className="cartItemPriceSlices">
+          {
+            cartItems.map((cartItem) => {
+              return (
+                <div className="cartItemPriceSlice">
+                  <p>{cartItem.title} x {cartItem.quantity}</p>
+                  <p>${cartItem.quantity * cartItem.price.amount}</p>
+                </div>
+              )
+            })
+          }
+        </div>
+        <div className="cartSubtotalSlice">
+          <p>{cartItems.reduce((total, curItem) => total + curItem.quantity * curItem.price.amount, 0)}</p>
+        </div>
+        <div className="cartTaxSlice">
+          <p>{TAX * cartItems.reduce((total, curItem) => total + curItem.quantity * curItem.price.amount, 0)}</p>
+        </div>
+        <div className="cartShippingSlice">
+          <p>FREE</p>
+        </div>
+        <div className="cartTotalSlice">
+          <p>{(1 + TAX) * cartItems.reduce((total, curItem) => total + curItem.quantity * curItem.price.amount, 0)}</p>
+        </div>
+        <div className="cartCheckoutButtonContainer">
+          <button>Checkout</button>
+        </div>
+      </div>
+    </>
   )
 }
 

8fd67b1d7c13083a4a7a7cb68bb8da20a435bae6
implexrr <implexrr@gmail.com>
2025-07-10 21:49:16 -0400

    refactor: cart item cards into CardItemCards.tsx, dollar signs in front of prices

---
+++ src/components/CartItemTotals.tsx
@@ -0,0 +1,43 @@
+import { useContext } from "react";
+
+import { ShopContext } from "../app/context/ShopContext";
+
+const TAX = 0.13;
+
+const CartItemTotals = () => {
+  const { cartItems } = useContext(ShopContext);
+  return (
+    <div className="cartItemTotals">
+      <div className="cartItemPriceSlices">
+        {
+          cartItems.map((cartItem) => {
+            return (
+              <div className="cartItemPriceSlice">
+                <p>{cartItem.title} x {cartItem.quantity}</p>
+                <p>${cartItem.quantity * cartItem.price.amount}</p>
+              </div>
+            )
+          })
+        }
+      </div>
+      <div className="cartSubtotalSlice">
+        <p>{cartItems.reduce((total, curItem) => total + curItem.quantity * curItem.price.amount, 0)}</p>
+      </div>
+      <div className="cartTaxSlice">
+        <p>{(TAX * cartItems.reduce((total, curItem) => total + curItem.quantity * curItem.price.amount, 0)).toFixed(2)}</p>
+      </div>
+      <div className="cartShippingSlice">
+        <p>FREE</p>
+      </div>
+      <div className="cartTotalSlice">
+        <p>{((1 + TAX) * cartItems.reduce((total, curItem) => total + curItem.quantity * curItem.price.amount, 0)).toFixed(2)}</p>
+      </div>
+      <div className="cartCheckoutButtonContainer">
+        <button onClick={() => {window.alert('nah b')}}>Checkout</button>
+      </div>
+    </div>
+  )
+}
+
+export default CartItemTotals
+
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -2,8 +2,7 @@ import { useContext } from "react";
 
 import { ShopContext } from "../app/context/ShopContext";
 import CartItemCards from "../components/CartItemCards";
-
-const TAX = 0.13;
+import CartItemTotals from "../components/CartItemTotals";
 
 // TODO: Refactor into cartItemCards component
 // TODO: Refactor into cartItemTotals component
@@ -13,35 +12,7 @@ const Cart = () => {
   return (
     <>
       <CartItemCards></CartItemCards>
-      <div className="cartItemTotals">
-        <div className="cartItemPriceSlices">
-          {
-            cartItems.map((cartItem) => {
-              return (
-                <div className="cartItemPriceSlice">
-                  <p>{cartItem.title} x {cartItem.quantity}</p>
-                  <p>${cartItem.quantity * cartItem.price.amount}</p>
-                </div>
-              )
-            })
-          }
-        </div>
-        <div className="cartSubtotalSlice">
-          <p>{cartItems.reduce((total, curItem) => total + curItem.quantity * curItem.price.amount, 0)}</p>
-        </div>
-        <div className="cartTaxSlice">
-          <p>{TAX * cartItems.reduce((total, curItem) => total + curItem.quantity * curItem.price.amount, 0)}</p>
-        </div>
-        <div className="cartShippingSlice">
-          <p>FREE</p>
-        </div>
-        <div className="cartTotalSlice">
-          <p>{(1 + TAX) * cartItems.reduce((total, curItem) => total + curItem.quantity * curItem.price.amount, 0)}</p>
-        </div>
-        <div className="cartCheckoutButtonContainer">
-          <button onClick={() => {window.alert('nah b')}}>Checkout</button>
-        </div>
-      </div>
+      <CartItemTotals></CartItemTotals>
     </>
   )
 }
